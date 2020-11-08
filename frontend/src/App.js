aad99e7422a574feaa263bad449b577cd21218d1
Arman <inzamam.arman@gmail.com>
2020-11-08 22:48:56 +0600

    showing a demo json product in screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import {Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-      Welcome to ezShop
+    <Header />
+      <main className="py-3"> 
+        <Container >
+        <HomeScreen />
+        </Container>
+      </main>
+    <Footer />
+
     </>
   );
 }
