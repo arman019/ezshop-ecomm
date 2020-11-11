9dfcd3532d3a784c43858acd656d150c6bc429d2
Arman <inzamam.arman@gmail.com>
2020-11-12 00:27:45 +0600

    custom error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from'express';
 import dotenv from'dotenv'
 import {connectDb} from './config/db.js'
 import  productRoutes from './routes/productRoutes.js'
+import {notFound,errorHandler} from './middlewares/errorMiddleware.js'
 
 //mongodb+srv://arman019:inzamamul@cluster0.nx4ee.mongodb.net/ezshop
 dotenv.config()
@@ -12,6 +13,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products',productRoutes);
+
+app.use(notFound);
+app.use(errorHandler)
 
 const PORT = process.env.PORT || '5000' 
 
