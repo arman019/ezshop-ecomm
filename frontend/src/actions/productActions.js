e7f2044ef01c579f057a919d7a20c68abfb194cb
Arman <inzamam.arman@gmail.com>
2020-11-15 16:01:54 +0600

    error message and loader

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,26 +1,27 @@
-import {
-    PRODUCT_LIST_FAIL,
-    PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_REQUEST
-} from '../constants/productConstants'
 import axios from 'axios'
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+} from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
-
     try {
-        dispatch({ type: PRODUCT_LIST_REQUEST });
-        const { data } = await axios('/api/products')
+        dispatch({ type: PRODUCT_LIST_REQUEST })
+        const { data } = await axios.get('/api/products')
+        console.log("data",data)
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        });
-
+            payload: data,
+        })
     } catch (error) {
-        console.log('error in catch ', error)
+        console.log("error in action",error)
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message ?
-                error.response.data.message : error.response
-        });
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
     }
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,41 @@
+  
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const { loading, error, products } = useSelector(state => state.productList)
+  const productList = useSelector((state) => state.productList)
+  const { loading ,products, error } = productList
+  console.log("error in homescreen",productList.error)
 
-    useEffect(() => {
-        dispatch(listProducts())
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
 
-    }, [dispatch])
-
-
-    return (
-        <>
-            <h3>Latest Products</h3>
-            {loading ? (<h3>Loading...</h3>) :
-                error ? (<h3>{error}</h3>) :
-                    (<Row>
-                        {products.map((product) => (
-                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                <Product product={product} />
-                            </Col>
-                        ))}
-                    </Row>)}
-
-        </>
-    )
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  )
 }
 
 export default HomeScreen
