ce7182e37576e9de88ee24c2742518c8d519ddbd
Arman <inzamam.arman@gmail.com>
2020-11-25 18:02:47 +0600

    placeOrder screens work except for the placeOrderHandler function whicn needs work for backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+
 
 
 const App = () => {
@@ -19,6 +21,7 @@ const App = () => {
     <Header />
       <main className="py-3"> 
           <Container >
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/profile' component={ProfileScreen} />
---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import { Form, Button, Card, List, ListGroup, Row, Col, Image } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Message from '../components/Message'
+import CheckOutSteps from '../components/CheckOutSteps'
+
+const PlaceOrderScreen = () => {
+    const cart = useSelector(state => state.cart)
+
+    const placeOrderHandler = (e) => {
+        //
+    }
+
+    return (
+        <>
+            <CheckOutSteps step1 step2 step3 />
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p>
+                                <strong>Address:</strong>&nbsp;
+                            {cart.shippingAddress.address},{cart.shippingAddress.city},{cart.shippingAddress.postalCode},{cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Payment Method</h2>
+                            <strong>Payment:</strong>&nbsp; {cart.paymentMethod}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Ordered Items</h2>
+                            {cart.cartItems.length === 0 ?
+                                (<Message>Cart is Empty</Message>) :
+                                (<ListGroup variant='flush'>
+
+                                    {cart.cartItems.map((item, i) => (
+                                        <ListGroup.Item key={i}>
+                                            <Row>
+                                                <Col md={2}>
+                                                    <Image src={item.image} alt={item.name} fluid rounded></Image>
+                                                </Col>
+                                                <Col>
+                                                    <Link to={`/product/${item.product}`}>
+                                                        {item.name}
+                                                    </Link>
+                                                </Col>
+                                                <Col md={5}>
+                                                    {item.qty} x ${item.price} = ${(item.price * item.qty).toFixed(2)}
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    ))}
+
+                                </ListGroup>)
+                            }
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+
+                <Col md={4} >
+                    <Card>
+                        <ListGroup varaint='flush'>
+                            <ListGroup.Item>
+                                <h2>Order Details </h2>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col> <strong>Total Items</strong></Col>
+                                    <Col className='mr-5'>{cart.cartItems.reduce((acc, item) => acc + item.qty, 0)}</Col>
+
+                                </Row>
+                            </ListGroup.Item>
+
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col> <strong>Total Cost</strong></Col>
+                                    <Col className='mr-5'>${cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)}</Col>
+
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button
+                                    type='button'
+                                    className=' btn-block'
+                                    disabled={cart.cartItems.length === 0}
+                                    onClick={placeOrderHandler}
+                                >
+                                    Place Order
+                                </Button>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Card>
+                </Col>
+
+            </Row>
+
+        </>
+    )
+}
+
+export default PlaceOrderScreen
