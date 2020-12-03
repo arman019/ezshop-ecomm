fd9ad9a64fcbbcff0429015c61d5cc8afa74cb14
Arman <inzamam.arman@gmail.com>
2020-12-03 15:50:10 +0600

    cart orderDetails and userDetails reset when logout

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SHIPPING_ADDRESS,CART_SAVE_PAYMENT_METHOD } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SHIPPING_ADDRESS,CART_SAVE_PAYMENT_METHOD, CART_RESET_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
 
@@ -38,7 +38,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
                 ...state,
                 paymentMethod: action.payload
             }
-    
+        case CART_RESET_ITEM:
+            return{
+                ...state,
+                cartItems:[]
+            }
 
         default:
             return state
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -12,6 +12,7 @@ import {
     ORDER_LIST_MY_FAIL,
     ORDER_LIST_MY_SUCCESS,
     ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_RESET,
 
   } from '../constants/orderConstants'
   
@@ -111,6 +112,10 @@ export const orderListMyReducer = (
       return {
         loading: false,
         error: action.payload,
+      }
+    case ORDER_LIST_MY_RESET:
+      return{
+        orders:[]
       }
     default:
       return state
