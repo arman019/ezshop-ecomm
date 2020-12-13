d2961098a61019cac40899315f08dffd7d9db5bd
Arman <inzamam.arman@gmail.com>
2020-12-13 21:04:18 +0600

    createProduct, updateProduct in productController in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import {getAllproducts,getProductById,deleteProductById} from '../controller/productController.js'
+import {getAllproducts,getProductById,deleteProductById,createProduct,updateProduct} from '../controller/productController.js'
 import { protect, adminAuth } from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
 router.get('/', getAllproducts)
 
+router.post('/', protect,adminAuth,createProduct)
+
 router.get('/:id',getProductById);
+
+router.put('/:id', protect, adminAuth, updateProduct);
 
 router.delete('/:id', protect,adminAuth,deleteProductById);
 
